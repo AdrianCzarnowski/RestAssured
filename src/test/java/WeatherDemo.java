b1a7639e052f7b6802d669f919e1df559218f41b
aczarnowski <adriannnczarnowski@gmail.com>
2022-05-18 08:47:22 +0200

    gitignore

--- src/test/java/WeatherDemo.java
+++ src/test/java/WeatherDemo.java
@@ -1,17 +1,13 @@
-import io.restassured.RestAssured;
-import io.restassured.response.Response;
-import org.json.JSONArray;
-import org.json.JSONObject;
 import org.testng.annotations.Test;
 
 
 import static io.restassured.RestAssured.*;
 import static java.lang.System.getProperty;
 
-public class JsonPlaceHolderDemo extends TestBase {
+public class WeatherDemo extends TestBase {
 
     @Test
-    public void weather2() {
+    public void weather() {
         given(getRequestSpecificationWeather()).
                 when()
                 .get(getProperty("BASE_URL") + getProperty("endpoint")).
